1f15b9a2a4b9a10d055bafb2886cd00edd29ce90
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2024-04-03 11:14:44 +0200

    Create Banalne zadania - poziom wejściówki.py

---
+++ 2E/python/sprawdziany/przeliczania-binarne/Banalne zadania - poziom wejściówki/Banalne zadania - poziom wejściówki.py
@@ -0,0 +1,36 @@
+# zad 1
+
+n = input("wprowadz liczbe binarna: ")
+if n[-1] == "0":
+    print ("wpisana liczba jest parzysta")
+else:
+    print ("wpisana liczba nie jest parzysta")
+print ()
+# zad 2
+a = input ("wprowadz pierwsza liczbe binarna: ")
+print ()
+b = input ("wprowadz druga liczbe binarna: ")
+print ()
+def parzystaSuma (a, b):
+    if (int(a[-1]) % 2 == 0 and int (b[-1]) == 0) or (int(a[-1]) % 2 != 0 and int(b[-1]) % 2 != 0):
+        print ("suma jest parzysta")
+    else:
+        print ("suma nie jest parzysta")
+    print ()
+
+def parzystaRoznica(a, b):
+    if (int(a[-1]) % 2 == 0 and int(b[-1])) or (int(a[-1]) % 2 != 0 and int(b[-1])):
+        print ("roznica jest parzysta")
+    else:
+        print ("roznicy nie jest parzysta")
+    print ()
+def parzystyIloczn(a, b):
+    if (int(a[-1]) % 2 == 0 and int(b[-1]) == 0) or (int(a[-1]) % 2 != 0 and int(b[-1])):
+        print ("iloczyna jest parzysty")
+    else:
+        print ("iloczyn nie jest parzysty")
+    print ()
+
+parzystaSuma (a, b)
+parzystaRoznica (a, b)
+parzystyIloczn(a, b)
