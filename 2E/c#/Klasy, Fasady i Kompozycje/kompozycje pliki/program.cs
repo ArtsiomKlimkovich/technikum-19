fe90951c13154475dcaa1110111bb158523d3bca
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2024-04-15 14:59:20 +0200

    Create program.cs

---
+++ 2E/c#/Klasy, Fasady i Kompozycje/kompozycje pliki/program.cs
@@ -0,0 +1,46 @@
+Katalog d1 = new Katalog("Dysk");
+Katalog k1 = new Katalog("Zdjecia");
+Katalog k2 = new Katalog("Praca");
+Katalog k3 = new Katalog("Rozne");
+
+d1.dodaj(k1);
+d1.dodaj(k2);
+d1.dodaj(k2);
+
+Plik z1 = new Plik("foto1");
+Plik z2 = new Plik("foto2");
+k1.dodaj(z1);
+k1.dodaj(z2);
+
+abstract class Wierzcholek {
+    public string name;
+    public abstract void wyswietl();
+}
+
+class Plik : Wierzcholek {
+    public Plik(string n) {
+        name = n;
+    }
+
+    public override void wyswietl() {
+        Console.WriteLine("--");
+        Console.WriteLine(name);
+    }
+}
+
+class Katalog : Wierzcholek {
+    List<Wierzcholek> wierzcholki = new List<Wierzcholek>();
+    public Katalog(string n) {
+        name = n;
+    }
+    public void dodaj (Wierzcholek wierzcholek) {
+        wierzcholki.Add(wierzcholek);
+    }
+    public override void wyswietl() {
+        Console.WriteLine(name);
+        foreach (var item in wierzcholki) {
+            Console.WriteLine("--");
+            item.wyswietl();
+        }
+    }
+}
