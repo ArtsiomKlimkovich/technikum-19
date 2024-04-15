b5e8f0f5368bf94ac0176a3deb94ef523bf87752
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2024-04-15 19:46:12 +0200

    Update zadania.py

--- 2E/python/sprawdziany/przeliczania-binarne/harcore zadania(impossible)/zadania.py
+++ 2E/python/sprawdziany/przeliczania-binarne/harcore zadania(impossible)/zadania.py
@@ -1,1 +1,19 @@
-
+# zad 1
+def printBinarySequence (n, prefix="", lastDigit=0, OneInRow=0):
+    if n == 0:
+        print (prefix)
+        return 1
+    
+    total = 0
+    
+    for digit in range (2):
+        if digit == 1 and lastDigit == 1:
+            continue
+        if digit == 1:
+            OneInRow += 1
+        else:
+            OneInRow = 0
+        total += printBinarySequence(n-1, prefix + str(digit),digit, OneInRow)
+    return total
+n = int(input("input bin num: "))
+print (f"dla {n} ilosc ciagow jest to {printBinarySequence(n)}")
