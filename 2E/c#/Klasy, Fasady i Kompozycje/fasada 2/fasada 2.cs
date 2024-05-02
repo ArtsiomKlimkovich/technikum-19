895dc60608f32544bd8402719310b06a2e05f164
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2024-05-02 19:36:50 +0200

    Update fasada 2.cs

--- 2E/c#/Klasy, Fasady i Kompozycje/fasada 2/fasada 2.cs
+++ 2E/c#/Klasy, Fasady i Kompozycje/fasada 2/fasada 2.cs
@@ -1,1 +1,35 @@
+// Fasada 2
+class Client {
+    WashingMachine washingMachine;
+    public void wash(WashingMachine washingMachine) {
+        this.washingMachine = washingMachine;
+    }
+}
 
+class Washing {
+    public void wash() { }
+}
+
+class Rinsing {
+    public void rinse() { }
+}
+
+class Spinning {
+    public void spin() { }
+}
+class WashingMachine {
+
+    Washing washing;
+    Rinsing rinsing;
+    Spinning spinning;
+
+    public void startWashing() { }
+
+    public void washThis() {
+        washing = new Washing();
+        rinsing = new Rinsing();
+        spinning = new Spinning();
+    }
+
+    public void method1() { }
+}
