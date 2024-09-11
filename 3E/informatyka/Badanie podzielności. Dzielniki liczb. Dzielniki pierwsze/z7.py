e02b8d90a40c7a017928c6eaf1241c5731257525
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2024-09-11 13:04:39 +0200

    Update z7.py

--- 3E/informatyka/Badanie podzielności. Dzielniki liczb. Dzielniki pierwsze/z7.py
+++ 3E/informatyka/Badanie podzielności. Dzielniki liczb. Dzielniki pierwsze/z7.py
@@ -1,3 +1,10 @@
+# Zadanie 7
+# Napisz program, który sprawdzi, czy liczba całkowita dodatnia n podana przez użytkownika jest liczbą
+# doskonałą. Liczba doskonała, to taka, która jest równa sumie swoich dzielników właściwych (czyli
+# mniejszych od tej liczby). Program wyświetli jeden z komunikatów „tak” lub „nie”. Przykładowe liczby
+# doskonałe:
+# 6 = 1+2+3
+# 28 = 1+2+4+7+14
 n = int(input ("input num: "))
 
 count = 0
