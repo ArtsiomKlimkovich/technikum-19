861e85a5a5917029811b0dad29cde308d3dd491c
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2024-10-02 15:36:54 +0100

    Add files via upload

---
+++ 3E/informatyka/wyszukiwanie binarne/z1.py
@@ -0,0 +1,18 @@
+def isInTable (T, a, n):
+    left = 0
+    right = n-1
+    while left < right:
+        middle = (left+right) // 2
+        if T[middle] < a:
+            left = middle + 1
+        else:
+            right = middle - 1
+    return T[left] == a
+
+nums = list(map(int, input("input sequence: ").split(" ")))
+a = int (input ("input a: "))
+n = 10
+if isInTable(nums, a, n):
+    print ("tak")
+else:
+    print ("nie")
