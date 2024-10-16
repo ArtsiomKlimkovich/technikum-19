81ec458824b64cc0fbbdcf66f1052516bf54012e
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2024-10-16 11:45:54 +0100

    Update z5.py

--- 3E/informatyka/wyszukiwanie binarne/z5.py
+++ 3E/informatyka/wyszukiwanie binarne/z5.py
@@ -1,5 +1,15 @@
-liczba=int(input("Podaj liczbe:"))
-cyfry = []
-while liczba>0:
-    reszta=liczba%10
-    cyfry.append(reszta)
+Funkcja CzyNalezy(a, n, T)
+    lewy = 1
+    prawy = n
+    
+    Podczas (lewy <= prawy)
+        srodek = (lewy + prawy) // 2
+        
+        Jeśli T[srodek] == a wtedy
+            Zwróć Prawda
+        Inaczej jeśli T[srodek] < a wtedy
+            lewy = srodek + 1
+        Inaczej
+            prawy = srodek - 1
+            
+    Zwróć Fałsz
