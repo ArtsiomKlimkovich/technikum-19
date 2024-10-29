831f128bb107a773117f65a97a82847f04b0a106
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2024-10-29 17:37:18 +0100

    Update z2.py

--- 3E/informatyka/Sito Eratostenesa(liczby pierwsze)/z2.py
+++ 3E/informatyka/Sito Eratostenesa(liczby pierwsze)/z2.py
@@ -1,25 +1,20 @@
 # Napisz program w języku Python, który wyświetli liczbę oraz sumę liczb pierwszych mniejszych lub równych n, gdzie
 # n jest liczbą naturalną większą od 1, wykorzystując algorytm sita Eratostenesa.
 def sito (n):
-    czy_pierwsza = []
-    for i in range (n+1):
-        czy_pierwsza.append(0)
-    czy_pierwsza[0] = czy_pierwsza[1] = 1
-    i = 2
-    while i * i <= n:
-        if czy_pierwsza[i] == 0:
-            for j in range (i*i, n+1, i):
-                czy_pierwsza[j] = 1
-        i += 1
-    return czy_pierwsza
+    sum = 0
+    count = 0
+    primes = [True] * (n+1)
+    p = 2
+    while p*p <= n:
+        if primes[p]:
+            for i in range (p*p, n+1, p):
+                primes[i] = False
+        p += 1
+    for i in range(2, n+1):
+        if primes[i]:
+            count += 1
+            sum += i
+    print ("ilosc - ", count, "; suma - ", sum)
 
-n = int (input ("input n: "))
-
-pierwsze = sito (n)
-
-sum = 0
-for i in range (n + 1):
-    if pierwsze[i] == 0:
-        sum += i
-
-print (sum)
+n = int(input ("input n: "))
+sito(n)
