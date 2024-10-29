e0f7e1caa1863891da2b81bf3235a8b154692824
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2024-10-29 17:00:25 +0100

    Update z1.py

--- 3E/informatyka/Sito Eratostenesa(liczby pierwsze)/z1.py
+++ 3E/informatyka/Sito Eratostenesa(liczby pierwsze)/z1.py
@@ -1,22 +1,16 @@
 # Napisz program w języku Python, który wyświetli wszystkie liczby pierwsze mniejsze lub równe n, gdzie n jest liczbą
 # naturalną większą od 1, wykorzystując algorytm sita Eratostenesa.
-def sito (n):
-    czy_pierwsza = []
-    for i in range (n+1):
-        czy_pierwsza.append(0)
-    czy_pierwsza[0] = czy_pierwsza[1] = 1
-    i = 2
-    while i * i <= n:
-        if czy_pierwsza[i] == 0:
-            for j in range (i*i, n+1, i):
-                czy_pierwsza[j] = 1
-        i += 1
-    return czy_pierwsza
-
-n = int (input ("input n: "))
-
-pierwsze = sito (n)
-
-for i in range (n + 1):
-    if pierwsze[i] == 0:
-        print (i, end=" ")
+def sito(n):
+    primes = [True] * (n+1)
+    p = 2
+    while p*p <= n:
+        if primes[p]:
+            for i in range (p*p, n+1, p):
+                primes[i] = False
+        p += 1
+    for i in range (2, n+1):
+        if primes[i]:
+            print (i, end=" ")
+        
+n = int(input ("input n: "))
+sito(n)
