9f32cfec55ae2e2672c35b95255b29fd99e7b988
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2025-04-15 15:30:39 +0200

    Update zadanie 2.py

--- 3E/informatyka/15. Operacje arytmetyczne wykonywane w różnych systemach liczbowych/zadanie 2.py
+++ 3E/informatyka/15. Operacje arytmetyczne wykonywane w różnych systemach liczbowych/zadanie 2.py
@@ -3,7 +3,7 @@ def suma_binarnych(a, b):
     b_decimal = int(b, 2)
     suma = a_decimal + b_decimal
     return bin(suma)[2:]
-
+ 
 a = input("Podaj pierwszą liczbę binarną: ")
 b = input("Podaj drugą liczbę binarną: ")
 
